c2734ed43c8e43f7c0f4441b51baad7a0f177b21
gialen <g.lentini88@gmail.com>
2023-04-14 12:20:12 +0200

    update

--- realsense2_camera/src/realsense_bridge.cpp
+++ realsense2_camera/src/realsense_bridge.cpp
@@ -4,17 +4,26 @@
 
 #include <rpwc_msgs/PCloudImageReq.h>
 #include <rpwc_msgs/PointCloudImage.h>
+#include <rpwc_msgs/cameraInfo.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
+#include <std_srvs/Empty.h>
 
 sensor_msgs::PointCloud2 point_cloud_;
 sensor_msgs::Image image_;
+sensor_msgs::CameraInfo cam_info_;
 
 bool callbackServerCamera(rpwc_msgs::PCloudImageReq::Request  &req, rpwc_msgs::PCloudImageReq::Response &res)
 {
     res.data.pointCloud = point_cloud_;
     res.data.image = image_;
+	return true;
+}
+
+bool callbackServerCameraInfo(rpwc_msgs::cameraInfo::Request  &req, rpwc_msgs::cameraInfo::Response &res)
+{
+	res.cameraInfo = cam_info_;
 	return true;
 }
 
@@ -26,6 +35,11 @@ void callback_image(const sensor_msgs::ImageConstPtr& msg)
 void callback_point_cloud (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
 	point_cloud_ = *cloud_msg;
+}
+
+void callback_camera_info (const sensor_msgs::CameraInfoConstPtr& info_msg)
+{
+	cam_info_ = *info_msg;
 }
 
 int main(int argc, char** argv)
@@ -40,10 +54,12 @@ int main(int argc, char** argv)
 	ros::Rate rate(5);
 
 	ros::ServiceServer serverCamera = nh.advertiseService("cameraRGBD", callbackServerCamera);
+	ros::ServiceServer serverCameraInfo = nh.advertiseService("camera/info", callbackServerCameraInfo);
 
     image_transport::ImageTransport it(nh);
 	image_transport::Subscriber sub_image = it.subscribe("camera/color/image_rect_color", 1, &callback_image);
     ros::Subscriber sub_point_cloud = nh.subscribe("camera/depth_registered/points", 1, &callback_point_cloud);
+    ros::Subscriber sub_camera_info = nh.subscribe("camera/color/camera_info", 1, &callback_camera_info);
 	ros::Publisher PCloudImage = nh.advertise<rpwc_msgs::PointCloudImage>("cameraRGBD", 1);
 	
 	while (ros::ok())
