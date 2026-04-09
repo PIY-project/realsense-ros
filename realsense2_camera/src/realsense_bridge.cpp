365040606cb38a393bca2934d8429b24d9f63179
EmaLoca <emanuele.locatelli@exsensia.ai>
2026-04-09 10:12:05 +0000

    [ADDED] Added new fake services to the bridge node;

--- realsense2_camera/src/realsense_bridge.cpp
+++ realsense2_camera/src/realsense_bridge.cpp
@@ -5,6 +5,9 @@
 #include <rpwc_msgs/PCloudImageReq.h>
 #include <rpwc_msgs/PointCloudImage.h>
 #include <rpwc_msgs/cameraInfo.h>
+#include <rpwc_msgs/setSettingsCamera.h>
+#include <rpwc_msgs/getSettingsCamera.h>
+#include <rpwc_msgs/suggestSettingsCamera.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
@@ -28,19 +31,40 @@ bool callbackServerCameraInfo(rpwc_msgs::cameraInfo::Request  &req, rpwc_msgs::c
 	return true;
 }
 
+bool callbackServerSetSettings(rpwc_msgs::setSettingsCamera::Request &req, rpwc_msgs::setSettingsCamera::Response &res)
+{
+	ROS_WARN("Funzione non supportata dalla camera!");
+	return true;
+}
+
+bool callbackServerGetSettings(rpwc_msgs::getSettingsCamera::Request& req, rpwc_msgs::getSettingsCamera::Response& res)
+{
+	ROS_WARN("Funzione non supportata dalla camera!");
+	return true;
+}
+
+bool callbackServerSuggestSettings(rpwc_msgs::suggestSettingsCamera::Request& req, rpwc_msgs::suggestSettingsCamera::Response& res)
+{
+	ROS_WARN("Funzione non supportata dalla camera!");
+	return true;
+}
+
 void callback_image(const sensor_msgs::ImageConstPtr& msg)
 {
-  image_ = msg;
+  	image_ = msg;
+	return;
 }
 
 void callback_point_cloud (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
 	point_cloud_ = cloud_msg;
+	return;
 }
 
 void callback_camera_info (const sensor_msgs::CameraInfoConstPtr& info_msg)
 {
 	cam_info_ = info_msg;
+	return;
 }
 
 int main(int argc, char** argv)
@@ -56,22 +80,16 @@ int main(int argc, char** argv)
 
 	ros::ServiceServer serverCamera = nh.advertiseService("sensorMeas", callbackServerCamera);
 	ros::ServiceServer serverCameraInfo = nh.advertiseService("camera/info", callbackServerCameraInfo);
+	ros::ServiceServer serverSetSettings = nh.advertiseService("set_settings_camera", callbackServerSetSettings);
+	ros::ServiceServer serverGetSettings = nh.advertiseService("get_settings_camera", callbackServerGetSettings);
+	ros::ServiceServer serverSuggestSettings = nh.advertiseService("suggest_settings_camera", callbackServerSuggestSettings);
 
     image_transport::ImageTransport it(nh);
 	image_transport::Subscriber sub_image = it.subscribe("camera/color/image_rect_color", 1, &callback_image);
     ros::Subscriber sub_point_cloud = nh.subscribe("camera/depth_registered/points", 1, &callback_point_cloud);
     ros::Subscriber sub_camera_info = nh.subscribe("camera/color/camera_info", 1, &callback_camera_info);
-	// ros::Publisher PCloudImage = nh.advertise<rpwc_msgs::PointCloudImage>("cameraRGBD", 1);
 	
-	// while (ros::ok())
-	// {
-	// 	rpwc_msgs::PointCloudImage tmp_data;
-	// 	tmp_data.pointCloud = *point_cloud_;
-	// 	tmp_data.image = *image_;
-	// 	PCloudImage.publish(tmp_data);
-	// 	ros::spinOnce();
-	// 	rate.sleep();
-	// }
 	ros::spin();
+
 	return 0;
 }
